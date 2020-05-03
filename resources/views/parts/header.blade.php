f9264d5971ac337b25eb79ed48a0d7ceae7ef0d5
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-03 15:47:26 +0200

    Fixed header responsive

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,24 +1,28 @@
-<header class="header">
-    <div class="header__logo">
-        Les Tréteaux
-    </div>
-
-    <div class="header__responsive">
-
-    </div>
-
-    <div class="header__content">
-        <div class="header__nav">
-            @include('parts.nav')
+<header class="header header--">
+    <div class="header__container wrapper">
+        <div class="header__logo">
+            Les Tréteaux
         </div>
 
-        <div class="header__icons">
-            <div class="header__search">
+        <div class="header__responsive">
+            <a href="#" class="header__burger">
+                <span class="header__lines"></span>
+            </a>
+        </div>
 
+        <div class="header__content">
+            <div class="header__nav">
+                @include('parts.nav')
             </div>
 
-            <div class="header__social">
-                @include('parts.social')
+            <div class="header__icons">
+                <div class="header__search">
+
+                </div>
+
+                <div class="header__social">
+                    @include('parts.social')
+                </div>
             </div>
         </div>
     </div>

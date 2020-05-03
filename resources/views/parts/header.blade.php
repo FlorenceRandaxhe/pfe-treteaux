5a450fe3ef22a9149406610602b0e962355fe59c
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-03 13:16:18 +0200

    Added icons

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <div class="header__logo">
+        Les Tréteaux
+    </div>
+
+    <div class="header__nav">
+        @include('parts.nav')
+    </div>
+
+    <div class="header__icons">
+        <div class="header__search">
+            @include('parts.search')
+        </div>
+
+        <div class="header__social">
+            @include('parts.social')
+        </div>
+    </div>
+</header>

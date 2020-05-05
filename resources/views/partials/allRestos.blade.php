c0fb4fbeae7184ebbbaa455a230c3badaeb0ba68
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-05 16:29:41 +0200

    Added rest css

--- resources/views/partials/allRestos.blade.php
+++ resources/views/partials/allRestos.blade.php
@@ -3,7 +3,7 @@
         <div class="allRestos__intro">
             <h2 class="allRestos__title">Les restaurants</h2>
             <p class="allRestos__text">
-                Nous vous proposons les offres suivantes :
+                Nous vous proposons les offres suivantes : <br>
                 Les réservations pour ces formules se feront uniquement au guichet du Centre Culturel (du lundi au vendredi de 8h30 à 17h00). Payement par carte bancaire.
             </p>
             <a href="#" class="btn btn--primary">Voir les spectacles concernés</a>

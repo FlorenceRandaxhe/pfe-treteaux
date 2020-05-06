5edea0a53b9466d14a1fc4bc43baa57baf2b0c80
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-06 15:12:44 +0200

    fixed tabs css

--- resources/views/parts/tabs.blade.php
+++ resources/views/parts/tabs.blade.php
@@ -1,16 +1,19 @@
 <div class="tabs">
     <ul class="tabs__list">
         <li class="tabs__item tabs__item--active">
-            <a class="tabs__link" href="#salle"></a>
-            <span class="tabs__text">La salle</span>
+            <a class="tabs__link" href="#salle">
+                <span class="tabs__text">La salle</span>
+            </a>
         </li>
         <li class="tabs__item">
-            <a class="tabs__link" href="#poly"></a>
-            <span class="tabs__text">Salle polyvalente</span>
+            <a class="tabs__link" href="#poly">
+                <span class="tabs__text">Salle polyvalente</span>
+            </a>
         </li>
         <li class="tabs__item">
-            <a class="tabs__link" href="#foyer"></a>
-            <span class="tabs__text">Foyer</span>
+            <a class="tabs__link" href="#foyer">
+                <span class="tabs__text">Foyer</span>
+            </a>
         </li>
     </ul>
 </div>

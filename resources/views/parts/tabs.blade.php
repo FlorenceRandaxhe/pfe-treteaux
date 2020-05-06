7b866ec90becc8c20e4f5e6bad89b181d81d39ee
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-06 10:27:56 +0200

    Added tabs css

--- resources/views/parts/tabs.blade.php
+++ resources/views/parts/tabs.blade.php
@@ -1,13 +1,16 @@
 <div class="tabs">
     <ul class="tabs__list">
-        <li class="tabs__item">
-            <a class="tabs__link" href="#salle">La salle</a>
+        <li class="tabs__item tabs__item--active">
+            <a class="tabs__link" href="#salle"></a>
+            La salle
         </li>
         <li class="tabs__item">
-            <a class="tabs__link" href="#poly">Salle polyvalente</a>
+            <a class="tabs__link" href="#poly"></a>
+            Salle polyvalente
         </li>
         <li class="tabs__item">
-            <a class="tabs__link" href="#foyer">Foyer</a>
+            <a class="tabs__link" href="#foyer"></a>
+            Foyer
         </li>
     </ul>
 </div>

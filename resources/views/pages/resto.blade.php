e27b09584776cad46d5d170b665bf0dfc8473458
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-05-07 18:47:18 +0200

    Fixed hero css

--- resources/views/pages/resto.blade.php
+++ resources/views/pages/resto.blade.php
@@ -1,6 +1,16 @@
 @extends('layout')
 @section('title', 'Offre resto')
 @section('main')
+    @include('partials.hero', [
+        'class' => 'left',
+        'color' => 'blue',
+        'title' => 'Profitez d\'un  repas avant votre spectacle',
+        'text' => 'Profitez d\'un succulent repas dans les restaurants du centre de Visé, à deux pas de la salle de spectacle, avant de venir apprécier l\'un de nos spectacles à prix réduit.',
+        'img' => 'img.jpg',
+        'url' => '',
+        'icon' => '',
+        'label' => ''
+    ])
     @include('partials.allRestos')
     @include('partials.info', [
         'class' => '',

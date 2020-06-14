155c30523a48763b096bbacf70626c37b3d3cebf
Florence Randaxhe <randaxhe.florence@gmail.com>
2020-06-14 10:13:23 +0200

    added video icon

--- resources/views/partials/eventGallery.blade.php
+++ resources/views/partials/eventGallery.blade.php
@@ -10,6 +10,7 @@
                 @foreach($event->galleryContent as $slide)
                     @if($slide->name() === 'video')
                     <div class="eventGallery__fig">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="eventGallery__svg"><polygon points="23 7 16 12 23 17 23 7"></polygon><rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect></svg>
                         <a href="#" class="eventGallery__imgLink">
                             <img class="eventGallery__img eventGallery__video" data-video="{{ $slide->id }}" src="{{ asset('storage/' . $slide->cover) }}">
                         </a>
